da6f938c5f5ffd9a27d9e22b4223a177cad844cc
Jayesh <jayeshpatil8him@gmail.com>
2026-02-05 05:53:38 +0530

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,15 +1,7 @@
 { pkgs }: {
   deps = [
     pkgs.python3
-    pkgs.python3Packages.pip
     pkgs.ffmpeg
     pkgs.git
   ];
-  
-  env = {
-    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      pkgs.ffmpeg
-      pkgs.stdenv.cc.cc.lib
-    ];
-  };
 }
